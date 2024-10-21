5af419a08ee2bb8a315e963cae5bbfca6bb346f0
SofiaBejar <150276443+SofiaBejar@users.noreply.github.com>
2024-10-21 06:27:52 +0200

    Add files via upload

---
+++ ejercicios/String/js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const logOutButton = document.getElementById("logOutButton");
+const boton = document.getElementById("boton");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", function () {
+    mostrar(resultado);
+});
+
+
+logOutButton.addEventListener("click", function (event) {
+    console.log("quieres cerrar sesión");
+    deleteLocalStorage("jota");
+    window.location.href = "../index.html";
+});
